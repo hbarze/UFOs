24c086fd63ed229d7d399011f205edbd1b75557a
Henry Barze <96043107+hbarze@users.noreply.github.com>
2022-05-20 11:47:00 -0500

    11.5.4 Commit

--- static/js/app.js
+++ static/js/app.js
@@ -22,3 +22,30 @@ function buildTable(data) {
       );
     });
   }
+
+// Setting up a new function, "HandleClick", using a D3.js 
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  };
+
+  //Listen for events that occur on a webpage
+  //tied to the filter button to be built on the webpage
+  d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
